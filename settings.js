0189aef6240fc897150716d3f15855ea83b2591a
cyberanuwa <109344829+cyberanuwa@users.noreply.github.com>
2022-07-15 14:20:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['948716465']
+global.premium = ['948716465']
+global.ownernomer = '948716465'
+global.ownername = '🐦Anupa Uthsara'
+global.botname = 'Cyber Anuwa BOT'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://www.youtube.com/channel/UCNiFH-La9LtCuq1F7QXTnxA'
+global.packname = 'Cyber Anuwa'
+global.author = '🐦Cyber Anuwa BOT'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
